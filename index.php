d5c57463b2e3b5440917e3fc325df066362e1fb4
jesusrds1 <jesusrds09@gmail.com>
2022-01-19 13:26:54 +0100

    Commit desde outra máquina

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
     echo "(\$x==10 and \$y==3) devolve ".$z."<br/>";
     $z= ($x==10 or $y==5);
     echo "(\$x==10 or \$y==5) devolve ".$z."<br/>";
-    ?>
+   //Comentariod ende outra maquina
+   ?>
 </body>
 </html>
